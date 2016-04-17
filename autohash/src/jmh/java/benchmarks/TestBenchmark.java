b02197c01e285d29421fe6c5c68aeffd3722806b
Dima Gerasimov <karlicos@yandex-team.ru>
2016-04-17 14:19:47 +0300

    restructure project, use maven plugin

--- autohash/src/jmh/java/benchmarks/TestBenchmark.java
+++ autohash/src/jmh/java/benchmarks/TestBenchmark.java
@@ -1,10 +1,16 @@
 package benchmarks;
 
 import javafx.util.Pair;
-import org.jetbrains.annotations.NotNull;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
-
 
 import java.util.ArrayList;
 import java.util.HashSet;
